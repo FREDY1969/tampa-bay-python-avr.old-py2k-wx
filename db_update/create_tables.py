40e8062e2f196521b18a6b602236391ede295720
Bruce Frederiksen <dangyogi@gmail.com>
2009-06-30 22:53:52 -0400

    Getting some of the db_update stuff working.

--- db_update/create_tables.py
+++ db_update/create_tables.py
@@ -2,6 +2,9 @@
 
 # create_tables.py db
 
+r'''Create all the latest version of tables in the database listed in db_table.
+'''
+
 from __future__ import with_statement
 import sys
 import os.path
@@ -17,7 +20,7 @@ def get_max_version(db_cur):
     return db_cur.fetchone()[0]
 
 def run():
-    global Backup_version, New_version
+    global New_version
 
     if len(sys.argv) != 2:
         sys.stderr.write("usage: create_tables.py db\n")
@@ -27,15 +30,11 @@ def run():
 
     with contextlib.closing(db.connect(db_file)) as db_conn:
         with contextlib.closing(db_conn.cursor()) as db_cur: 
-            Backup_version = get_backup_version(db_file)
             New_version = get_max_version(db_cur)
-            db_cur.execute("""select new_table.id, new_table.table_name
-                                from db_table as new_table
-                               where new_table.version = ?
-                                 and not exists(select id from db_table
-                                                where id = new_table.id
-                                                  and version = ?)
-                           """, (New_version, Backup_version))
+            db_cur.execute("""select id, table_name
+                                from db_table
+                               where version = ?
+                           """, (New_version,))
             try:
                 for id, table_name in db_cur.fetchall():
                     create_table(id, table_name, db_cur)
@@ -45,17 +44,23 @@ def run():
             db_conn.commit()
 
 def create_table(id, table_name, db_cur):
-    db_cur.execute("""select col_name, definition from db_column
+    #print "create_table:", id, table_name
+    db_cur.execute("""select col_name, definition
+                        from db_column
                        where table_id = ? and version = ?
                        order by position
                    """, (id, New_version))
+    lines = []
     for name, defn in db_cur:
+        #print "name:", repr(name)
+        #print "defn:", repr(defn)
         if name is not None:
             lines.append("    %s %s" % (name, defn))
         else:
             lines.append("    %s" % (defn,))
-    command += ");"
-    db_cur.execute("create table %s (\n%s\n)" % (table_name, ',\n'.join(lines)))
+    cmd = "create table %s (\n%s\n)" % (table_name, ',\n'.join(lines))
+    #print "cmd:", cmd
+    db_cur.execute(cmd)
 
 
 if __name__ == "__main__":
