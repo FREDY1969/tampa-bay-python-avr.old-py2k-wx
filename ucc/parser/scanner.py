d66f7cbb421fbdb7b85cc8d44aa69c6467462ee6
Bruce Frederiksen <dangyogi@gmail.com>
2009-07-15 16:15:04 -0400

    Scanner starting to work.

--- ucc/parser/scanner.py
+++ ucc/parser/scanner.py
@@ -4,10 +4,9 @@
 
 from __future__ import with_statement, division
 
+import math
+import os.path
 from ply import lex
-import word
-import domain
-import number
 
 debug = 0
 
@@ -18,107 +17,98 @@ states = (
 )
 
 tokens = (
+    'APPROX_NUMBER_TOK',
     'CHAR_TOK',
     'DEINDENT_TOK',
     'INDENT_TOK',
+    'INTEGER_TOK',
+    'LB_TOK',
     'LP_TOK',
     'NAME_TOK',
     'NEWLINE_TOK',
-    'NUMBER_TOK',
-    'START_PARAMS_TOK',
+    'RATIO_TOK',
     'START_SERIES_TOK',
     'STRING_TOK',
     'WORD_TOK',
 )
 
-literals = ')]'
+literals = '()[]'
 
 t_ignore = ' '
 
-t_ignore_comment = r'(?<![^ \r\n])\#.*'
-
-def t_continuation(t):
-    r'''(?<!\\)                 # can't come after another backslash!
-        \\                      # backslash
-        (?:\ \ *(?:\#.*)?)?     # optional spaces, optional comment
-        (?:\r)?\n               # newline
-    '''
-    t.lexer.lineno += 1
+t_ignore_comment = r'\#.*'
 
 def t_START_SERIES_TOK(t):
     r''':                       # :
         (?:\ \ *(?:\#.*)?)?     # optional spaces, optional comment
         (?:\r)?\n               # newline
     '''
+    t.lexer.lineno += 1
     t.lexer.begin('colonindent')
-    t.lexer.skip(-1)
+    t.lexer.skip(-1)            # push back the final '\n'
     return t
 
-last_colonindent = 0
+Last_colonindent = 0
 
 t_colonindent_ignore = ''
 
-def t_colonindent_sp(t):
-    r'\n\ *'
-    indent = len(t.value) - 1
-    if indent % 4:
-        raise SyntaxError("improper indent level, must be multiple of 4",
-                          syntaxerror_params())
-    if indent > last_colonindent:
-        if indent != last_colonindent + 4:
-            raise SyntaxError("multiple indent", syntaxerror_params())
-        last_colonindent = indent
-        t.lexer.begin('INITIAL')
-        return 'INDENT_TOK'
-    if indent < last_colonindent:
-        if indent < last_colonindent - 4:
-            t.lexer.skip(-len(t.value))         # play it again, Sam...
-        else:
-            t.lexer.begin('INITIAL')
-        last_colonindent -= 4
-        return 'DEINDENT_TOK'
-    t.lexer.begin('INITIAL')
+def t_colonindent_blank_line(t):
+    r'\n(?:\ *(?:\#.*)?(?:\r)?\n)+'
+    t.lexer.lineno += t.value.count('\n') - 1
 
-def t_NEWLINE_TOK(t):
-    r'''(?:\r)?\n                       # newline
-        (?:\ *(?:\#.*)?(?:\r)?\n)*      # blank lines
-    '''
-    t.lexer.lineno += t.value.count('\n')
-    t.lexer.begin('indent')
-    t.lexer.skip(-1)
+def t_colonindent_INDENT_TOK(t):
+    r'\n\ *'
+    global Last_colonindent
+    indent = len(t.value) - 1
+    if indent != Last_colonindent + 4:
+        raise SyntaxError("improper indent level after :", syntaxerror_params())
+    Last_colonindent = indent
+    t.lexer.begin('INITIAL')
     return t
 
-indent_levels = []
-last_indent = 0
+def t_NEWLINE_TOK(t):
+    r'(?:\r)?\n'                       # newline
+    global Sent_newline
+    t.lexer.begin('indent')
+    t.lexer.skip(-1)                   # push back the final '\n'
+    Sent_newline = False
 
 t_indent_ignore = ''
 
+def t_indent_blank_line(t):
+    r'\n(?:\ *(?:\#.*)?(?:\r)?\n)+'
+    t.lexer.lineno += t.value.count('\n') - 1
+    t.lexer.skip(-1)                   # push back the final '\n'
+
 def t_indent_sp(t):
     r'\n\ *'
-    global indent_levels, last_indent
+    global Last_colonindent, Sent_newline
     indent = len(t.value) - 1
-    try:
-        level = indent_levels.index(indent)
-    except ValueError:
-        if indent == 0 and not indent_levels:
-            indent_levels.append(0)
-            last_indent = 0
-            t.lexer.begin('INITIAL')
-            return
-        raise SyntaxError("invalid indent level, "
-                            "doesn't match anything on previous line",
+    if indent == Last_colonindent:
+        t.lexer.lineno += 1
+        t.lexer.begin('INITIAL')
+        t.type = 'NEWLINE_TOK'
+        return t
+    if indent > Last_colonindent:
+        t.lexer.lineno += 1
+        t.lexer.begin('INITIAL')
+        return
+    if indent % 4:
+        raise SyntaxError("invalid indent level, must be multiple of 4 spaces",
                           syntaxerror_params())
-    if level > last_indent:
-        if level == last_indent + 1:
-            t.lexer.begin('INITIAL')
-        last_indent += 1
-        return 'INDENT_TOK'
-    if indent < last_indent:
-        if level == last_indent - 1:
-            t.lexer.begin('INITIAL')
-        last_indent -= 1
-        return 'DEINDENT_TOK'
-    t.lexer.begin('INITIAL')
+    if not Sent_newline:
+        t.lexer.lineno += 1
+        t.lexer.skip(-len(t.value))     # come back here after NEWLINE_TOK
+        t.type = 'NEWLINE_TOK'
+        Sent_newline = True
+        return t
+    t.type = 'DEINDENT_TOK'
+    Last_colonindent -= 4
+    if indent == Last_colonindent:
+        t.lexer.begin('INITIAL')
+    else:
+        t.lexer.skip(-len(t.value))     # come back here after DEINDENT_TOK
+    return t
 
 def t_CHAR_TOK(t):
     r"'[^\\]'"
@@ -136,139 +126,244 @@ escapes = {
 }
 
 def t_escaped_char_tok(t):
-    r"'\\[^x]'"
-    t.value = ord(escapes.get(t.value[2], t.value[2]))
+    r"'\\[^xX]'"
+    t.value = ord(escapes.get(t.value[2].lower(), t.value[2]))
     t.type = 'CHAR_TOK'
     return t
 
 def t_hex_char_tok(t):
-    r"'\\x[0-9a-fA-F]{2}'"
-    t.value = int(t.value[3:], 16)
+    r"'\\[xX][0-9a-fA-F]{2}'"
+    t.value = int(t.value[3:-1], 16)
     t.type = 'CHAR_TOK'
     return t
 
 def t_start_string(t):
     r'"'
-    global string_value
+    global String_value
     t.lexer.begin('string')
-    string_value = ""
-
-def t_string_end(t):
-    r'"'
-    t.value = string_value
-    t.lexer.begin('INITIAL')
-    t.type = 'STRING_TOK'
-    return t
+    String_value = ""
 
 t_string_ignore = ''
 
 def t_string_char(t):
     r'[^\\"]'
-    string_value += t.value[1]
+    String_value += t.value[0]
 
 def t_string_escaped_char(t):
-    r'\\[^x]'
-    string_value += escapes.get(t.value[2], t.value[2])
+    r'\\[^xX]'
+    String_value += escapes.get(t.value[1].lower(), t.value[1])
 
 def t_string_hex_char(t):
-    r'\\x[0-9a-fA-F]{2}'
-    string_value += int(t.value[3:], 16)
+    r'\\[xX][0-9a-fA-F]{2}'
+    String_value += int(t.value[2:], 16)
+
+def t_string_STRING_TOK(t):
+    r'"'
+    t.value = String_value
+    t.lexer.begin('INITIAL')
+    return t
 
 def t_LP_TOK(t):
-    r'''(?<![^[( ])     # preceeded by [, (, or space, or at start of line
+    r'''(?<![^[( ])     # preceded by [, (, or space, or at start of line
         \(
     '''
     return t
 
-def t_START_PARAMS_TOK(t):
-    r'\('
-    return t
-
 def t_LB_TOK(t):
-    r'''(?<![^[( ])     # preceeded by [, (, or space, or at start of line
+    r'''(?<![^[( ])     # preceded by [, (, or space, or at start of line
         \[
     '''
     return t
 
-def t_START_SUBSCRIPT_TOK(t):
-    r'\['
-    return t
-
-def t_NUMBER_TOK(t):
-    r'''(?<![^[( ])     # preceeded by [, (, or space, or at start of line
-        [0-9]+
-        (?:\.[0-9]+)?   # optional decimal point
-        /[0-9]*         # denominator
+def t_INTEGER_TOK(t):
+    r'''[0-9]+
         (?=[]) \r\n])   # followed by ], ), space or newline
     '''
-    t.value = number.any_precision.from_str(t.value, 10).to_domain()
+    t.value = int(t.value)
     return t
 
-def t_HEX_NUMBER_TOK(t):
-    r'''(?<![^[( ])         # preceeded by [, (, or space, or at start of line
-        0[xX][0-9a-fA-F]+
-        (?:\.[0-9a-fA-F]+)? # optional decimal part
-        /[0-9a-fA-F]*       # denominator
-        (?=[]) \r\n])       # followed by ], ), space or newline
+def t_hex_INTEGER_TOK(t):
+    r'''0[xX][0-9]+
+        (?=[]) \r\n])   # followed by ], ), space or newline
     '''
-    t.value = number.any_precision.from_str(t.value, 16).to_domain()
-    t.type = 'NUMBER_TOK'
+    t.value = int(t.value, 16)
+    return t
+
+def t_RATIO_TOK(t):
+    r'''[0-9]+/[0-9]+ # ratio
+        (?=[]) \r\n])   # followed by ], ), space or newline
+    '''
+    slash = t.value.index('/')
+    t.value = (int(t.value[:slash]), int(t.value[slash+1:]))
+    return t
+
+def t_ratio_2(t):
+    r'''[0-9]+
+        \.[0-9]+        # decimal point
+        /[0-9]+         # denominator
+        (?=[]) \r\n])   # followed by ], ), space or newline
+    '''
+    dot = t.value.index('.')
+    slash = t.value.index('/')
+    denominator = int(t.value[slash+1:])
+    t.value = (int(t.value[:dot]) * denominator + int(t.value[dot+1:slash]),
+               denominator)
+    t.type = 'RATIO_TOK'
+    return t
+
+def t_ratio_3(t):
+    r'''[0-9]+
+        \.[0-9]+        # decimal point
+        /               # implied denominator
+        (?=[]) \r\n])   # followed by ], ), space or newline
+    '''
+    dot = t.value.index('.')
+    denominator = 10**(len(t.value) - dot - 1)
+    t.value = (int(t.value[:dot]) * denominator + int(t.value[dot + 1:-1]),
+               denominator)
+    t.type = 'RATIO_TOK'
+    return t
+
+def t_ratio_4(t):
+    r'''\.[0-9]+        # decimal point
+        /               # implied denominator
+        (?=[]) \r\n])   # followed by ], ), space or newline
+    '''
+    denominator = 10**(len(t.value) - 2)
+    t.value = (int(t.value[1:-1]), denominator)
+    t.type = 'RATIO_TOK'
+    return t
+
+def t_hex_RATIO_TOK(t):
+    r'''0[xX][0-9]+/[0-9]+ # ratio
+        (?=[]) \r\n])        # followed by ], ), space or newline
+    '''
+    slash = t.value.index('/')
+    t.value = (int(t.value[:slash], 16), int(t.value[slash+1:], 16))
+    t.type = 'RATIO_TOK'
+    return t
+
+def t_hex_ratio_2(t):
+    r'''0[xX][0-9]+
+        \.[0-9]+        # decimal point
+        /[0-9]+         # denominator
+        (?=[]) \r\n])   # followed by ], ), space or newline
+    '''
+    dot = t.value.index('.')
+    slash = t.value.index('/')
+    denominator = int(t.value[slash + 1:], 16)
+    t.value = (int(t.value[:dot], 16) * denominator +
+                 int(t.value[dot + 1:slash], 16),
+               denominator)
+    t.type = 'RATIO_TOK'
+    return t
+
+def t_hex_ratio_3(t):
+    r'''0[xX][0-9]+
+        \.[0-9]+        # decimal point
+        /               # implied denominator
+        (?=[]) \r\n])   # followed by ], ), space or newline
+    '''
+    dot = t.value.index('.')
+    denominator = 16**(len(t.value) - dot - 1)
+    t.value = (int(t.value[:dot], 16) * denominator +
+                 int(t.value[dot + 1:-1], 16),
+               denominator)
+    t.type = 'RATIO_TOK'
+    return t
+
+def t_hex_ratio_4(t):
+    r'''0[xX]
+        \.[0-9]+        # decimal point
+        /               # implied denominator
+        (?=[]) \r\n])   # followed by ], ), space or newline
+    '''
+    denominator = 16**(len(t.value) - 4)
+    t.value = (int(t.value[3:-1], 16), denominator)
+    t.type = 'RATIO_TOK'
     return t
 
 def t_APPROX_NUMBER_TOK(t):
-    r'''(?<![^[( \r\n])  # preceeded by [, (, or space, or at start of line
-        [0-9]+
-        (?: \.[0-9]+ (?: ~[0-9])?
-          | ~[0-9]+)?
+    # All decimal forms with a '.':
+    r'''(?: [0-9]+ \.[0-9]* | \.[0-9]+ )
+        (?: ~[0-9] )?
+        (?: [eE] [+-]?[0-9]+ )?
         (?=[]) \r\n])    # followed by ], ), space or newline
     '''
-    t.value = number.fixed_precision.from_str(t.value, 10).to_domain()
-    t.type = 'NUMBER_TOK'
+    t.value = approx(t.value.lower())
     return t
 
-def t_HEX_APPROX_NUMBER_TOK(t):
-    r'''(?<![^[( ])      # preceeded by [, (, or space, or at start of line
-        0[xX][0-9a-fA-F]+
-        (?: \.[0-9a-fA-F]+ (?: ~[0-9a-fA-F])?
-          | ~[0-9a-fA-F]+)?
+def t_approx_2(t):
+    # All decimal forms without a '.':
+    r'''[0-9]+ (?: ~[0-9] )? [eE] [+-]?[0-9]+
         (?=[]) \r\n])    # followed by ], ), space or newline
     '''
-    t.value = number.fixed_precision.from_str(t.value, 16).to_domain()
-    t.type = 'NUMBER_TOK'
+    t.value = approx(t.value.lower())
+    t.type = 'APPROX_NUMBER_TOK'
+    return t
+
+def t_hex_APPROX_NUMBER_TOK(t):
+    # All hex forms with a '.':
+    r'''0[xX] (?: [0-9a-fA-F]+ \.[0-9a-fA-F]* | \.[0-9a-fA-F]+ )
+              (?: ~[0-9a-fA-F] )?
+              (?: [xX] [+-]?[0-9]+ )?
+        (?=[]) \r\n])    # followed by ], ), space or newline
+    '''
+    t.value = approx(t.value[2:].lower(), 16)
+    t.type = 'APPROX_NUMBER_TOK'
+    return t
+
+def t_hex_approx_2(t):
+    # All hex forms without a '.':
+    r'''0[xX] [0-9a-fA-F]+ (?: ~[0-9a-fA-F] )? [xX] [+-]?[0-9]+
+        (?=[]) \r\n])    # followed by ], ), space or newline
+    '''
+    t.value = approx(t.value[2:].lower(), 16)
+    t.type = 'APPROX_NUMBER_TOK'
     return t
 
 def t_NAME_TOK(t):
-    r'''(?<![^[( -])      # preceeded by [, (, -, or space, or at start of line
-        [^][() "'\r\n#]+
-        [^][() "'\r\n]*
-        (?=[][() \r\n])   # followed by [, ], (, ), space or newline
+    r'''[^[( \r\n"'#-]     # first character
+        [^[( \r\n]*        # middle characters
+        [^])[( \r\n]       # last character
     '''
-    if t.value in dictionary:
-        t.value = self.dictionary[t.value]
+    if t.value in Word_dict:
+        t.value = Word_dict[t.value]
         t.type = 'WORD_TOK'
     return t
 
-def t_negate(t):
-    r'''(?<![^[( -])       # preceeded by [, (, -, or space, or at start of line
-        -
-        (?=[^][() "'\r\n]) # followed by name or number
+def t_NAME_TOK_1(t):
+    r'''[^])[( \r\n"'#-]
+        (?=[][() \r\n])    # followed by [, ], (, ), space or newline
     '''
-    if 'negate' in dictionary:
-        t.value = dictionary['negate']
+    if t.value in Word_dict:
+        t.value = Word_dict[t.value]
         t.type = 'WORD_TOK'
     else:
         t.type = 'NAME_TOK'
     return t
 
-def t_minus(t):
-    r'''(?<![^ ])       # preceeded by space or at start of line
-        -
-        (?=[ \r\n])     # followed by space or newline
+def t_negate(t):
+    r'''-
+        (?=[^]) \r\n])  # followed by name, number, string, -, ( or [
     '''
-    if '-' in dictionary:
-        t.value = dictionary['-']
+    if 'negate' in Word_dict:
+        t.value = Word_dict['negate']
         t.type = 'WORD_TOK'
     else:
+        t.value = 'negate'
+        t.type = 'NAME_TOK'
+    return t
+
+def t_minus(t):
+    r'''-
+        (?=[]) \r\n])   # followed by space, newline, ] or )
+    '''
+    if '-' in Word_dict:
+        t.value = Word_dict['-']
+        t.type = 'WORD_TOK'
+    else:
+        t.value = '-'
         t.type = 'NAME_TOK'
     return t
 
@@ -277,36 +372,117 @@ def t_ANY_error(t):
                         t.value[0],
                       syntaxerror_params())
 
+def approx(s, base = 10):
+    r'''Return the approx value for s as (integer, binary_pt).
+
+    The format for s is: digit* .digit* ~digit e[+-]?digit*
+    Note that this does not accept negative numbers.
+
+    >>> approx('123')
+    (123, 0)
+    >>> approx('123~1')
+    (123, 0)
+    >>> approx('122~1')
+    (61, 1)
+    >>> approx('123.5')
+    (1976, -4)
+    >>> approx('123.5~1')
+    (988, -3)
+    >>> approx('123~1e1')
+    (77, 4)
+    >>> approx('123e1')
+    (308, 2)
+    >>> approx('123~1e-1')
+    (197, -4)
+    >>> approx('123e-1')
+    (197, -4)
+    '''
+    dot = s.find('.')
+    tilde = s.find('~')
+    e = s.find('e' if base == 10 else 'x')
+    if e < 0:
+        exp = 0
+        e = len(s)
+    else:
+        exp = int(s[e + 1:])
+    if tilde < 0:
+        prec = base // 2
+        prec_place = 1
+        tilde = e
+    else:
+        prec = int(s[tilde + 1:e], base)
+        prec_place = 0
+    if dot < 0:
+        decimal_places = 0
+        n = int(s[:tilde], base)
+    else:
+        decimal_places = tilde - dot - 1
+        n = int(s[:dot], base) * base**decimal_places + \
+            int(s[dot + 1:tilde], base)
+
+    number = n * base**(exp - decimal_places)
+    precision = prec / base**(decimal_places + prec_place - exp)
+
+    #print "n", n, "decimal_places", decimal_places, \
+    #      "prec", prec, "prec_place", prec_place, "exp", exp
+    #
+    #print "number", number
+    #print "precision", precision
+
+    # We want the greatest integer binary_pt such that:
+    #
+    #    (i - 0.5) * 2**binary_pt >= number - precision
+    #    (i + 0.5) * 2**binary_pt <= number + precision
+    #
+    # for some integer, i.
+    #
+    # This can not be solved directly.
+    #
+    # So we'll take an iterative approach starting with:
+    #
+    #    2**binary_pt * 0.5 == precision
+    #
+    # Solving for binary_pt:
+    #
+    #    binary_pt = log2(2 * precision)
+    #    binary_pt = log2(precision) + 1
+
+    target = math.log(precision) / math.log(2) + 1
+    binary_pt = int(math.ceil(target))
+    smallest_reject = None
+    largest_ok = None
+
+    while smallest_reject is None or largest_ok is None:
+        i = int(round(number / 2**binary_pt))
+        if (i - 0.5) * 2**binary_pt >= number - precision and \
+           (i + 0.5) * 2**binary_pt <= number + precision:
+            largest_ok = i, binary_pt
+            binary_pt += 1
+        else:
+            smallest_reject = binary_pt
+            binary_pt -= 1
+    return largest_ok
+
 def syntaxerror_params():
     pos = lexer.lexpos
-    start = pos
+    start = lexer.lexdata.rfind('\n', 0, pos) + 1
     lineno = lexer.lineno
-    while start > 0 and (start >= len(lexer.lexdata) or
-                         lexer.lexdata[start] in '\r\n'):
-        start -= 1
-    end = start
-    if debug: print "pos", pos, "lineno", lineno, "start", start
-    start = max(lexer.lexdata.rfind('\r', 0, start),
-                lexer.lexdata.rfind('\n', 0, start)) + 1
     column = pos - start + 1
-    end1 = lexer.lexdata.find('\r', end)
-    end2 = lexer.lexdata.find('\n', end)
-    if end1 < 0: end = end2
-    elif end2 < 0: end = end1
-    else: end = min(end1, end2)
-    if debug: print "start", start, "column", column, "end", end
+    end = lexer.lexdata.find('\n', start)
+    if end < 0: end = len(lexer.lexdata)
+    if debug:
+        print "pos", pos, "lineno", lineno, "column", column, \
+              "start", start, "end", end
     return (lexer.filename, lineno, column, lexer.lexdata[start:end])
 
 lexer = None
 
 def init(this_module, word_dict, debug_param, check_tables = False):
-    global last_colonindent, indent_levels, last_indent, debug, lexer
-    global dictionary
-    last_colonindent = 0
-    indent_levels = []
-    last_indent = 0
+    global Last_colonindent, debug, lexer
+    global Word_dict
+    Last_colonindent = 0
     debug = debug_param
-    dictionary = word_dict
+    Word_dict = word_dict
     if lexer is None:
         if debug_param:
             lexer = lex.lex(module=this_module, debug=1)
@@ -336,15 +512,15 @@ def tokenize(this_module, word_dict, s):
     r'''
         >>> import scanner
         >>> scanner.tokenize(scanner, {}, '22\n')
-        LexToken(NUMBER_TOK,<constant 22: <approx 22-22 .1 ~1>>,1,0)
+        LexToken(INTEGER_TOK,22,1,0)
         LexToken(NEWLINE_TOK,'\n',1,2)
         >>> scanner.tokenize(scanner, {},
         ...                  '# comment1\n\n    \n    # comment2\n44\n')
-        LexToken(NEWLINE_TOK,'\n\n    \n    # comment2\n',1,10)
-        LexToken(NUMBER_TOK,<constant 44: <approx 44-44 .1 ~1>>,5,32)
+        LexToken(NEWLINE_TOK,'\n',4,31)
+        LexToken(INTEGER_TOK,44,5,32)
         LexToken(NEWLINE_TOK,'\n',5,34)
     '''
-    init(this_module, word_dict, 1, True)
+    init(this_module, word_dict, 0, True)
     lexer.filename = 'tokenize'
     lexer.lineno = 1
     lexer.input(s)
