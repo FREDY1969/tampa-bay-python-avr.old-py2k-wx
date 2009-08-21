4a6c545f73089240ca649c417ccbefc75086d811
Bruce Frederiksen <dangyogi@gmail.com>
2009-08-21 16:41:50 -0400

    A few minor bug fixes.  ucc/ast/dump.py now can do one word.

--- ucc/ast/dump.py
+++ ucc/ast/dump.py
@@ -15,23 +15,30 @@ id: label word kind int1 int2 str1 str2 expect type_id
 from __future__ import with_statement
 
 import itertools
-import contextlib
 import os.path
 import sqlite3 as db
 
 Db_filename = "ast.db"
 
-def dump(project_dir):
-    with contextlib.closing(db.connect(os.path.join(project_dir,
-                                                    Db_filename))) \
-      as db_conn:
-        with contextlib.closing(db_conn.cursor()) as db_cur:
-            db_cur.execute("""select id from ast
-                               where word_body_id is null
-                               order by word""")
-            for id, in db_cur.fetchall():
-                print
-                dump_word(id, db_cur)
+class db_cursor(object):
+    def __init__(self, project_dir):
+        self.project_dir = project_dir
+    def __enter__(self):
+        self.db_conn = db.connect(os.path.join(self.project_dir, Db_filename))
+        self.db_cur = self.db_conn.cursor()
+        return self.db_cur
+    def __exit__(self, exc_type, exc_value, exc_tb):
+        #print "closing db connection"
+        self.db_cur.close()
+        self.db_conn.close()
+
+def dump(db_cur):
+    db_cur.execute("""select id from ast
+                       where word_body_id is null
+                       order by word""")
+    for id, in db_cur.fetchall():
+        print
+        dump_word(id, db_cur)
 
 def dump_word(id, db_cur, indent = ''):
     db_cur.execute("""
@@ -99,9 +106,20 @@ if __name__ == "__main__":
     import sys
 
     def usage():
-        sys.stderr.write("usage: dump.py project_dir\n")
+        sys.stderr.write("usage: dump.py [project_dir | file.ucl]\n")
         sys.exit(2)
 
     len(sys.argv) == 2 or usage()
 
-    dump(sys.argv[1])
+    if sys.argv[1].lower().endswith('.ucl'):
+        project_dir, file = os.path.split(sys.argv[1])
+        with db_cursor(project_dir) as db_cur:
+            db_cur.execute("""select id
+                                from ast
+                               where kind = 'word_body' and word = ?
+                           """,
+                           (file[:-4],))
+            dump_word(db_cur.fetchone()[0], db_cur)
+    else:
+        with db_cursor(sys.argv[1]) as db_cur:
+            dump(db_cur)
