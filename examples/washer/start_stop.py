378b05e01f7868290f427914f09a0664cecbbf24
spielberg <spielberg@spielbergs-powerbook-g4-15.local>
2009-09-12 17:20:19 -0400

    fixed a few issues w/ word imports need to get the gui running again

---
+++ examples/washer/start_stop.py
@@ -0,0 +1,6 @@
+# start_stop.py
+
+from examples.washer import declaration
+
+class start_stop(declaration.high_level_word):
+    pass
---
+++ examples/washer/translate.py
@@ -0,0 +1,7 @@
+# translate.py
+
+from examples.washer import declaration
+
+class translate(declaration.word):
+    pass
+
