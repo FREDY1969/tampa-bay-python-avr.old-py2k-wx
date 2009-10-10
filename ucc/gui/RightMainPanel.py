e93519bb240e86956586ed3d36b87f80a15320aa
Linden Wright <linden.wright@gmail.com>
2009-10-10 13:53:39 -0400

    Adding scroll bar functionality to top right pane

--- ucc/gui/RightMainPanel.py
+++ ucc/gui/RightMainPanel.py
@@ -9,26 +9,40 @@ import setpath
 setpath.setpath(__file__)
 
 import wx, wx.py
+import wx.lib.scrolledpanel as scrolled
+
 from ucc.gui.Registry import Registry
 
 class RightMainPanel(wx.Panel):
     def __init__(self, parent, id, style):
         wx.Panel.__init__(self, parent, id, style=style)
         
+        # setup splitter
+        
+        splitter = self.splitter = wx.SplitterWindow(self, wx.ID_ANY, style=wx.SP_3D)
+
         # setup panels
         
-        topPanel = self.topPanel = wx.Panel(self, wx.ID_ANY)
+        topPanel = self.topPanel = scrolled.ScrolledPanel(splitter, wx.ID_ANY)
         topSizer = self.topSizer = wx.BoxSizer(wx.VERTICAL)
         topPanel.SetSizer(topSizer)
-        self.bottomText = wx.py.editwindow.EditWindow(self, wx.ID_ANY,
+        topPanel.SetAutoLayout(1)
+        topPanel.SetupScrolling()
+        bottomText = self.bottomText = wx.py.editwindow.EditWindow(splitter, wx.ID_ANY,
                                                       style=wx.TE_MULTILINE)
         self.bottomText.setDisplayLineNumbers(True)
         
         # setup sizer
         
         sizer = wx.BoxSizer(wx.VERTICAL)
-        sizer.Add(topPanel, 1, wx.EXPAND)
-        sizer.Add(self.bottomText, 1, wx.EXPAND)
+
+        # add top and bottom stuff
+
+        splitter.SetMinimumPaneSize(200)
+        splitter.SetSashGravity(1)
+        splitter.SplitHorizontally(topPanel, bottomText, 300)
+
+        sizer.Add(splitter, 1, wx.EXPAND)
         
         self.SetSizer(sizer)
         sizer.Fit(self)
