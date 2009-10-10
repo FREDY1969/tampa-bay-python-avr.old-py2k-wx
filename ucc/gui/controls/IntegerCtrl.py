ece8d4c7775acbe8c3b064b1f9399aa374e52ce7
Bruce Frederiksen <dangyogi@gmail.com>
2009-10-10 13:23:57 -0400

    Automated merge with file:///home/bruce/python/arduino/Python-AVR/google

---
+++ ucc/gui/controls/IntegerCtrl.py
@@ -0,0 +1,13 @@
+r'''Control for integer word values'''
+
+class IntegerCtrl(object):
+    def __init__(self):
+        self.value = None
+        self.paintControl()
+    
+    def paintControl(self):
+        
+        
+    
+    def getValue(self):
+        return self.value
---
+++ ucc/gui/controls/StringCtrl.py
@@ -0,0 +1,13 @@
+r'''Control for string word values'''
+
+class StringCtrl(object):
+    def __init__(self):
+        self.value = None
+        self.paintControl()
+    
+    def paintControl(self):
+        
+        
+    
+    def getValue(self):
+        return self.value
