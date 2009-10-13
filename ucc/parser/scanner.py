e1a7778235d8b8f70d33fa51d728d6de8eeb68a6
Bruce Frederiksen <dangyogi@gmail.com>
2009-10-13 11:59:43 -0400

    Moved number parsing from scanner.py to ucc/parser/number.py

--- ucc/parser/scanner.py
+++ ucc/parser/scanner.py
@@ -4,8 +4,7 @@
 
 from __future__ import with_statement, division
 
-import math
-from ucc.parser import scanner_init
+from ucc.parser import scanner_init, number
 
 debug = 0
 
@@ -294,7 +293,7 @@ def t_APPROX_NUMBER(t):
         (?: [eE] [+-]?[0-9]+ )?
         (?=[]) \r\n])    # followed by ], ), space or newline
     '''
-    t.value = approx(t.value.lower())
+    t.value = number.approx(t.value).int_exp()
     return t
 
 def t_approx_2(t):
@@ -302,7 +301,7 @@ def t_approx_2(t):
     r'''[0-9]+ (?: (?: ~[0-9] )? [eE] [+-]?[0-9]+ | ~[0-9] )
         (?=[]) \r\n])    # followed by ], ), space or newline
     '''
-    t.value = approx(t.value.lower())
+    t.value = number.approx(t.value).int_exp()
     t.type = 'APPROX_NUMBER'
     return t
 
@@ -313,7 +312,7 @@ def t_hex_APPROX_NUMBER(t):
               (?: [xX] [+-]?[0-9]+ )?
         (?=[]) \r\n])    # followed by ], ), space or newline
     '''
-    t.value = approx(t.value[2:].lower(), 16)
+    t.value = number.approx(t.value).int_exp()
     t.type = 'APPROX_NUMBER'
     return t
 
@@ -324,7 +323,7 @@ def t_hex_approx_2(t):
           | ~[0-9a-fA-F] )
         (?=[]) \r\n])    # followed by ], ), space or newline
     '''
-    t.value = approx(t.value[2:].lower(), 16)
+    t.value = number.approx(t.value).int_exp()
     t.type = 'APPROX_NUMBER'
     return t
 
@@ -410,100 +409,6 @@ def t_ANY_error(t):
                         t.value[0],
                       scanner_init.syntaxerror_params())
 
-def approx(s, base = 10):
-    r'''Return the approx value for s as (integer, binary_pt).
-
-    The format for s is: digit* .digit* ~digit e[+-]?digit*
-    Note that this does not accept negative numbers.
-
-    >>> approx('123.')
-    (123, 0)
-    >>> approx('123~1')
-    (123, 0)
-    >>> approx('122~1')
-    (61, 1)
-    >>> approx('123.5')
-    (1976, -4)
-    >>> approx('123.5~1')
-    (988, -3)
-    >>> approx('123~1e1')
-    (77, 4)
-    >>> approx('123e1')
-    (308, 2)
-    >>> approx('123~1e-1')
-    (197, -4)
-    >>> approx('123e-1')
-    (197, -4)
-    '''
-    dot = s.find('.')
-    tilde = s.find('~')
-    e = s.find('e' if base == 10 else 'x')
-    if e < 0:
-        exp = 0
-        e = len(s)
-    else:
-        exp = int(s[e + 1:])
-    if tilde < 0:
-        prec = base // 2
-        prec_place = 1
-        tilde = e
-    else:
-        prec = int(s[tilde + 1:e], base)
-        prec_place = 0
-    if dot < 0:
-        decimal_places = 0
-        n = int(s[:tilde], base)
-    else:
-        decimal_places = tilde - dot - 1
-        if dot > 0:
-            n = int(s[:dot], base) * base**decimal_places
-        else:
-            n = 0
-        if decimal_places:
-            n += int(s[dot + 1:tilde], base)
-
-    number = n * base**(exp - decimal_places)
-    precision = prec / base**(decimal_places + prec_place - exp)
-
-    #print "n", n, "decimal_places", decimal_places, \
-    #      "prec", prec, "prec_place", prec_place, "exp", exp
-    #
-    #print "number", number
-    #print "precision", precision
-
-    # We want the greatest integer binary_pt such that:
-    #
-    #    (i - 0.5) * 2**binary_pt >= number - precision
-    #    (i + 0.5) * 2**binary_pt <= number + precision
-    #
-    # for some integer, i.
-    #
-    # This can not be solved directly.
-    #
-    # So we'll take an iterative approach starting with:
-    #
-    #    2**binary_pt * 0.5 == precision
-    #
-    # Solving for binary_pt:
-    #
-    #    binary_pt = log2(2 * precision)
-    #    binary_pt = log2(precision) + 1
-
-    target = math.log(precision) / math.log(2) + 1
-    binary_pt = int(math.ceil(target))
-    smallest_reject = None
-    largest_ok = None
-
-    while smallest_reject is None or largest_ok is None:
-        i = int(round(number / 2**binary_pt))
-        if (i - 0.5) * 2**binary_pt >= number - precision and \
-           (i + 0.5) * 2**binary_pt <= number + precision:
-            largest_ok = i, binary_pt
-            binary_pt += 1
-        else:
-            smallest_reject = binary_pt
-            binary_pt -= 1
-    return largest_ok
 
 def init(debug_param, token_dict = {}):
     r'''
