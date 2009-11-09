03fd616d973798c7019af31af883257012ce2afa
Bruce Frederiksen <dangyogi@gmail.com>
2009-11-09 16:34:41 -0500

    Moved all command line programs to scripts.
    Also created command line versions of load.py and compile.py.
    
    Renamed ucc/gui/start.py to scripts/ucc.py.

--- scripts/peek.py
+++ scripts/peek.py
@@ -1,13 +1,20 @@
-#!/usr/bin/env python
+#!/usr/bin/python
+
+from __future__ import with_statement
 
 import sys
 
+def usage():
+    sys.stderr.write("usage: peek.py file.hex\n")
+    sys.exit(2)
+
 if __name__ == "__main__":
-    sys.stdin = open('flash.hex','r')
-    for line in sys.stdin:
-        templine = line[0:3] + ' ' + line[3:7] + ' ' + line[7:9] + '  ' 
-        j = 9
-        while j < len(line)-4:
-            templine = templine + line[j+2:j+4] + line[j:j+2] + ' '
-            j+=4
-        print(templine)
+    if len(sys.argv) != 2: usage()
+    with open(sys.argv[1], 'r') as input:
+        for line in input:
+            templine = line[0:3] + ' ' + line[3:7] + ' ' + line[7:9] + '  ' 
+            j = 9
+            while j < len(line)-4:
+                templine = templine + line[j+2:j+4] + line[j:j+2] + ' '
+                j += 4
+            print(templine)
