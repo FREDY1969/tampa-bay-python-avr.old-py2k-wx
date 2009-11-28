b1e3866d3cc2ac41fb361d164bd1ae299027d970
Jason Keene <jasonkeene@gmail.com>
2009-11-28 17:38:23 -0500

    StringCtrl changes.

--- ucc/gui/controls/StringCtrl.py
+++ ucc/gui/controls/StringCtrl.py
@@ -5,7 +5,7 @@ from ucc.gui.controls.BaseCtrl import BaseCtrl
 
 class StringCtrl(BaseCtrl):
     def paintControl(self, parent):
-        self.t1 = wx.TextCtrl(self, -1, size=(125, -1))
+        self.t1 = wx.TextCtrl(parent, -1, size=(125, -1), pos=(20,0))
         self.Bind(wx.EVT_TEXT, self.TextChange, self.t1)
         
     def setInitialValue(self):
