92a65934a094b32a507366e499726db3b70d9fba
Jason Keene <jasonkeene@gmail.com>
2009-11-28 17:02:45 -0500

    New StringCtrl.

--- ucc/gui/controls/StringCtrl.py
+++ ucc/gui/controls/StringCtrl.py
@@ -1,7 +1,16 @@
 r'''Control for string word values'''
 
+import wx
 from ucc.gui.controls.BaseCtrl import BaseCtrl
 
 class StringCtrl(BaseCtrl):
     def paintControl(self, parent):
-        super(StringCtrl, self).paintControl(parent)
+        self.t1 = wx.TextCtrl(self, -1, size=(125, -1))
+        self.Bind(wx.EVT_TEXT, self.TextChange, self.t1)
+        
+    def setInitialValue():
+        self.t1.SetValue(self.answer_getter())
+    
+    def TextChange(self, event):
+        print event.GetString()
+        self.answer_setter(event.GetString())
