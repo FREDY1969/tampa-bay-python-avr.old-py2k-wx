1f5d2f75620c7fa8dbab3fd2fd081f2051ee5edc
Bruce Frederiksen <dangyogi@gmail.com>
2009-12-05 17:54:05 -0500

    SeriesCtrl starting to work, but StaticBox still wrong.

--- ucc/gui/controls/SeriesCtrl.py
+++ ucc/gui/controls/SeriesCtrl.py
@@ -4,6 +4,7 @@ r'''Control for series word values'''
 
 import wx
 from ucc.gui.controls.BaseCtrl import BaseCtrl
+import ucc.gui.controls
 
 class SeriesCtrl(BaseCtrl):
     def paintControl(self, parent):
@@ -13,28 +14,29 @@ class SeriesCtrl(BaseCtrl):
 
         self.children = []
         for sub_question in self.question.subquestions:
-            self.internal_sizer.Add(wx.StaticText(self.box, wx.ID_ANY,
-                                                  sub_question.label))
+            labelText = wx.StaticText(parent, wx.ID_ANY, sub_question.label)
+            self.internal_sizer.Add(labelText)
             if not hasattr(sub_question, 'control'):
                 msg = "<%s %s> has no 'control'" % \
                         (sub_question.__class__.__name__, sub_question.name)
                 print msg
-                self.internal_sizer.Add(wx.StaticText(self.box, wx.ID_ANY, msg))
+                self.internal_sizer.Add(wx.StaticText(parent, wx.ID_ANY, msg))
             else:
-                cls = getattr(getattr(controls, sub_question.control),
+                cls = getattr(getattr(ucc.gui.controls, sub_question.control),
                               sub_question.control)
                 child = \
-                  cls(self.box,
+                  cls(parent,
                       sub_question,
-                      lambda: getattr(self.answer_getter(), sub_question.name),
+                      lambda sub_question=sub_question:
+                          getattr(self.answer_getter(), sub_question.name),
                       self.error_setter)
                 self.internal_sizer.Add(child)
                 self.children.append(child)
 
         border = wx.BoxSizer()
         border.Add(self.internal_sizer, 1, wx.EXPAND | wx.ALL, 25)
-        self.SetSizer(border)
-        
+        parent.SetSizer(border)
+
     def error_setter(self, new_ans):
         raise AssertionError(
                 "%s.SeriesCtrl: answer_setter used by subordinate" %
