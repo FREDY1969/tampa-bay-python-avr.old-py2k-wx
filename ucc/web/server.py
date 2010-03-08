375b3c561e5d14964ca160ee8fd69126fbc7ce4a
Bruce Frederiksen <dangyogi@gmail.com>
2010-03-08 09:53:58 -0500

    Added webbrowser call, fixed POST bug in wsgi_app.py.

--- ucc/web/server.py
+++ ucc/web/server.py
@@ -3,11 +3,14 @@
 from doctest_tools import setpath
 setpath.setpath(__file__, remove_first = True)
 
-from ucc.web import wsgi_app
 from wsgiref.simple_server import make_server
+import webbrowser
+from ucc.web import wsgi_app
 
 httpd = make_server('', 8000, wsgi_app.wsgi_app)
 print "Serving HTTP on port 8000..."
+
+webbrowser.open('http://localhost:8000', 2)
 
 # Respond to requests until process is killed
 httpd.serve_forever()
